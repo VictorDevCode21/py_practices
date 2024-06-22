d3441596aef83d3f0e66b389edaa9ce0a21b9dd9
victor <victorenrique2002@gmail.com>
2024-06-22 02:12:28 -0400

    dicts and lists methods almost finished

---
+++ data_structures/dicts.py
@@ -0,0 +1,42 @@
+# Dictionaries
+
+name_dict = {
+            "Principal_key":
+            { 
+            "key": 2, 
+            "key2": "Victor"
+            },
+        }
+
+## get method
+get_key = name_dict.get("Principal_key")
+
+# Way of accessing a dictionary with the key and the value
+# print(name_dict["Principal_key"]["key2"])
+
+# print(get_key)
+
+
+
+## Iterating through a dictionary
+# for element in get_key:
+#     print(element)
+    
+## keys() method
+# Iterating through a dictionary with keys() method "To know the keys"
+# for key in name_dict.keys():
+#     print(key)
+
+## items() method
+# name_dict.items()
+
+# Iterating through a dictionary with items() method
+# for key, value in name_dict.items():
+#     print(key, value)
+
+# print(name_dict.items())
+
+## values() method
+# Iterating through a dictionary with values() method
+# for value in get_key.values():
+#     print(value)
